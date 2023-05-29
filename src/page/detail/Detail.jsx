a5081b9cb8fb0756004417fff254008d5eb25dfd
Kullanıcı Adınız <email@adresiniz.com>
2023-05-29 15:44:23 +0300

    detail css düzenlendi

--- src/page/detail/Detail.jsx
+++ src/page/detail/Detail.jsx
@@ -33,24 +33,26 @@ export default function Detail() {
           <div>
             <img className={styles.flags} src={country.flags.png} alt="" />
           </div>
-          <div>Name of country: {country.name.common}</div>
-          <div>Capital: {country.capital}</div>
-          <div>Traffic Flow Direction: {country.car?.side}</div>
-          <div>Continent: {country.region}</div>
-          <div>
-            Currencies: {Object.values(country.currencies)[0].name} Symbol:{" "}
-            {Object.values(country.currencies)[0].symbol}
-          </div>
-          {/* <div>
+          <div className={styles.countryInfo}>
+            <div>Name of country: {country.name.common}</div>
+            <div>Capital: {country.capital}</div>
+            <div>Traffic Flow Direction: {country.car?.side}</div>
+            <div>Continent: {country.region}</div>
+            <div>
+              Currencies: {Object.values(country.currencies)[0].name} Symbol:{" "}
+              {Object.values(country.currencies)[0].symbol}
+            </div>
+            {/* <div>
           Currencies: {country.currencies?.FOK?.name} Symbol:{" "}
           {country.currencies?.FOK?.symbol}
         </div> */}
-          <div>
-            Languages: {Object.values(country.languages)[0]} /{" "}
-            {Object.values(country.languages)[1]}
+            <div>
+              Languages: {Object.values(country.languages)[0]} /{" "}
+              {Object.values(country.languages)[1]}
+            </div>
+            <div>Population: {country.population}</div>
+            <div>{country.timezones}</div>
           </div>
-          <div>Population: {country.population}</div>
-          <div>{country.timezones}</div>
         </div>
 
         {country.coatOfArms.svg ? (
